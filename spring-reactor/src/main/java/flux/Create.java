ebaf44dfba3c1188aef7d8259d95d8b964314463
KKamtte <swye199@gmail.com>
2026-06-10 15:24:40 +0900

    create: sink를 통해서 데이터를 생성

---
+++ spring-reactor/src/main/java/flux/Create.java
@@ -0,0 +1,18 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+
+public class Create {
+    public static void main(String[] args) {
+        // create: sink를 통해서 데이터를 생성한다.
+        Flux<String> created = Flux.create(sink -> {
+            sink.next("First");
+            sink.next("Second");
+            sink.next("Third");
+            sink.next("Fourth");
+            sink.complete();
+        });
+
+        created.subscribe(System.out::println);
+    }
+}
