f4ff197e833e0467e2a459bf0452fe8e1a6dc3c8
KKamtte <swye199@gmail.com>
2026-06-10 15:59:41 +0900

    chaining: Flux 변환

---
+++ spring-reactor/src/main/java/flux/Chaining.java
@@ -0,0 +1,39 @@
+package flux;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Chaining {
+
+    public static void main(String[] args) {
+        Flux<String> upperCase = Flux.just("apple", "orange", "banana")
+                .map(String::toUpperCase);
+        upperCase.subscribe(System.out::println);
+
+        Flux<Integer> length = Flux.just("hello", "world", "reactor")
+                .map(String::toUpperCase)
+                .map(String::length);
+        length.subscribe(System.out::println);
+
+        Flux<String> result = Flux.just("1", "2", "3")
+                .flatMap(userId -> findUserName(userId));
+        result.subscribe(System.out::println);
+
+        Flux<String> expanded = Flux.just("apple", "orange", "banana")
+                .flatMap(word -> Flux.fromArray(word.split("")));
+        expanded.subscribe(System.out::println);
+
+        Flux<Integer> even = Flux.range(1, 10)
+                .filter(i -> i % 2 == 0);
+        even.subscribe(System.out::println);
+
+        Flux<String> startA = Flux.just("apple", "avocado", "banana", "apricot", "orange")
+                .filter(s -> s.startsWith("a"))
+                .map(String::toUpperCase);
+        startA.subscribe(System.out::println);
+    }
+
+    static Mono<String> findUserName(String userId) {
+        return Mono.just("User-" + userId);
+    }
+}
