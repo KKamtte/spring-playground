95703af0d6e544e060889584aec4572ac796b72e
KKamtte <swye199@gmail.com>
2026-06-10 22:35:23 +0900

    스티림 원소 변환: 비동기 변환에서 속도와 순서 보장 간의 트레이드오프

---
+++ spring-reactor/src/main/java/flux/process/Map.java
@@ -0,0 +1,26 @@
+package flux.process;
+
+import reactor.core.publisher.Flux;
+
+public class Map {
+    public static void main(String[] args) {
+        Flux.just(
+                        new User("Kim", 30),
+                        new User("Lee", 30),
+                        new User("Park", 30))
+                .map(user -> user.name)
+                .subscribe(System.out::println);
+
+        // 중간에 실패시 종료된다.
+        Flux.just("1", "2", "abc", "3")
+                .map(s -> Integer.parseInt(s))
+                .subscribe(
+                        data -> System.out.println(data),
+                        err -> System.out.println(err),
+                        () -> System.out.println("done")
+                );
+    }
+
+    record User(String name, int age) {
+    }
+}
