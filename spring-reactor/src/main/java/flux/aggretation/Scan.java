052bce1b134a5610843dc7e94614c9c853e63d01
KKamtte <swye199@gmail.com>
2026-06-15 18:18:20 +0900

    집계 연산자

---
+++ spring-reactor/src/main/java/flux/aggretation/Scan.java
@@ -0,0 +1,17 @@
+package flux.aggretation;
+
+import reactor.core.publisher.Flux;
+
+public class Scan {
+    public static void main(String[] args) {
+        // reduce: 최종 결과만 출력
+        Flux.just(1,2,3,4,5)
+                .reduce((acc, next) -> acc + next)
+                .subscribe(System.out::println);
+
+        // scan: 중간 누적값을 매 단계마다 방출 → 1, 3, 6, 10, 15 출력
+        Flux.just(1,2,3,4,5)
+                .scan((acc, next) -> acc + next)
+                .subscribe(System.out::println);
+    }
+}
