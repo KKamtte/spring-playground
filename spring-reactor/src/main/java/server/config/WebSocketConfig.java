8a3c982a5b6b3718c9042ad30ee89def6fc90c0d
KKamtte <swye199@gmail.com>
2026-06-17 18:09:07 +0900

    WebSocket 수립 과정

---
+++ spring-reactor/src/main/java/server/config/WebSocketConfig.java
@@ -0,0 +1,47 @@
+package server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import server.handler.EchoWebSocketHandler;
+
+import java.util.Map;
+
+/*
+    --> GET /ws/echo HTTP/1.1
+
+    Host: localhost:8080
+    Upgrade: websocket
+    Connection: Upgrade
+    Sec-WebSocket-Key: ...
+    Sec-WebSocket-Version: 13
+
+    --> 101 Switching Protocols HTTP/1.1
+
+    Host: localhost:8080
+    Upgrade: websocket
+    Connection: Upgrade
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public HandlerMapping webSocketHandlerMapping(EchoWebSocketHandler echoWebSocketHandler) {
+        Map<String, WebSocketHandler> map = Map.of("/ws/echo", echoWebSocketHandler);
+
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setUrlMap(map);
+        mapping.setOrder(-1);
+
+        return mapping;
+    }
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+}
---
+++ spring-reactor/src/main/java/server/handler/EchoWebSocketHandler.java
@@ -0,0 +1,18 @@
+package server.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+@Component
+public class EchoWebSocketHandler implements WebSocketHandler {
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        return session.send(
+                session.receive()
+                        .map(msg -> session.textMessage("Echo: " + msg.getPayloadAsText()))
+        );
+    }
+}
